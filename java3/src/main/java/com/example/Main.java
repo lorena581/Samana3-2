11c7d15ed5014be87d79f9c1bd9952370f759f3e
Michel <lorenaarango581@gmail.com>
2025-03-20 22:10:09 -0500

    creacion de estructura base del poyecto

---
+++ java3/src/main/java/com/example/Main.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+  
+           Calculadora calc = new Calculadora();
+           int resultado = calc.sumar(5, 3);
+           System.out.println("El resultado es: " + resultado);
+       }
+   }
+    
